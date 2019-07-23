f283b9a0d9d53987c20cf684f7725e2a86c3ced5
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 14:11:30 -0500

    se agrega configuracion para usar balanceo de carga con rest template

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -3,7 +3,6 @@ package com.formacionbdi.springboot.app.item.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
