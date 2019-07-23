d21696a4f7091169b6978ac35469849327994fa9
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 15:17:34 -0500

    se agrega configuracion de eureka-client para que los microservicios se registren automaticamente en el servidor de eureka

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,7 +2,7 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /*
@@ -10,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * permite inyectar los clientes en los controladores o cualquier componente.
  */
 @EnableFeignClients
+
 /*
  * Con esto se habilita el balanceo de carga del lado del cliente. Hay dos
  * anotaciones @RibbonClient y @RibbonClients una es para un cliente y la otra
@@ -17,8 +18,18 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * el name le indicas el microservicio al que requieres conectarte tanto Feign
  * como Ribbon se integran para hacer el balance de carga.
  * 
+ * 
+ * Se comenta ya que ahora es eureka quien administra toda esta conifguración
  */
-@RibbonClient(name = "servicio-productos")
+//@RibbonClient(name = "servicio-productos")
+
+/*
+ * Con esta anotación se habilita este proyecto como cliente de eureka; en el
+ * video dice que no es obligatorio hacerlo ya que con el solo hecho de tener la
+ * dependencia se configura pero recomienda hacerlo
+ */
+@EnableEurekaClient
+
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
