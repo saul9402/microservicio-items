3c9c10234421f7b839baebf814f68ad476369870
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 12:43:34 -0500

    se agrega implemetación de consumo de servicio con feign

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,7 +2,13 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*
+ * Habilitamos los clientes feign que se tengan implementados en el proyecto y
+ * permite inyectar los clientes en los controladores o cualquier componente.
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
---
+++ src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -0,0 +1,24 @@
+package com.formacionbdi.springboot.app.item.clientes;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.formacionbdi.springboot.app.item.models.Producto;
+
+//Se indica que esto es un cleinte feign
+//en el "name" indicas a que servicio te vas a conectar, 
+//este nombre se define en el application.yml del servicio a conectarse
+@FeignClient(name = "servicio-productos", url = "localhost:8001")
+public interface ProductoClienteRest {
+
+	// aqui es donde se define el metodo y la ruta del servicio que va a consumir
+	// este metodo
+	@GetMapping(value = "/listar")
+	public List<Producto> listar();
+
+	@GetMapping(value = "/ver/{id}")
+	public Producto detalle(@PathVariable Long id);
+}
